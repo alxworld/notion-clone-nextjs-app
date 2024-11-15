703cdb301c0ffdf085b9adc23d04ed3efc3a0344
alxworld <jsalex@gmail.com>
2024-11-16 00:09:41 +0530

    build fix 4

--- src/app/doc/[id]/layout.tsx
+++ src/app/doc/[id]/layout.tsx
@@ -3,11 +3,12 @@ import { auth } from '@clerk/nextjs/server'
 import RoomProvider from '@/components/RoomProvider'
 import * as React from 'react'
 
+type tParams = Promise<{ id: string }>
+
 //function DocLayout({ children, params: { id } }: { children: React.ReactNode; params: { id: string } }) {
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function DocLayout({ children, params }: { children: React.ReactNode; params: any }) {
+async function DocLayout({ children, params }: { children: React.ReactNode; params: tParams }) {
   auth.protect() // this redirects to /sign-in if not authenticated
-  const { id } = React.use(params)
+  const { id } = await params
   return <RoomProvider roomId={id}>{children}</RoomProvider>
 }
 export default DocLayout
