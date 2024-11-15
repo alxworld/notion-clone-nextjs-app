83b9938ea1054c350870bded6b2048c24414ac34
alxworld <jsalex@gmail.com>
2024-11-16 00:19:17 +0530

    build fix trails 1

--- src/app/doc/[id]/page.tsx
+++ src/app/doc/[id]/page.tsx
@@ -3,9 +3,11 @@ import * as React from 'react'
 
 import Document from '@/components/Document'
 
-// function DocumentPage({ params: { id } }): { params: { id: string } } {
-function DocumentPage({ params }): { params } {
-  const { id } = React.use(params)
+type tParams = { id: string }
+
+//function DocumentPage({ params: { id } }): { params: { id: string } ): JSX.Element {
+function DocumentPage({ params: tParams }: { params: tParams }): JSX.Element {
+  const { id } = tParams
   return (
     <div className="flex flex-col flex-1 min-h-screen">
       <Document id={id} />
