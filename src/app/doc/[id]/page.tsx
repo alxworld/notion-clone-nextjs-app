d760c8a4f88ba9f0bcbc9cc4c296e90937356530
alxworld <jsalex@gmail.com>
2024-11-16 00:41:45 +0530

    fix 3

--- src/app/doc/[id]/page.tsx
+++ src/app/doc/[id]/page.tsx
@@ -3,8 +3,21 @@ import * as React from 'react'
 
 import Document from '@/components/Document'
 
-function DocumentPage({ params: { id } }: { params: { id: string } }): JSX.Element {
+type tParams = Promise<{ id: string }>
+
+function DocumentPage({ params }: { params: tParams }): JSX.Element {
   //function DocumentPage({ params: tParams }: { params: tParams }): JSX.Element {
+  //const idRef = React.useRef<string | null>(null)
+  let id: string = ''
+
+  React.useEffect(() => {
+    async function fetchData() {
+      const fetchedId = await params
+      id = fetchedId
+    }
+    fetchData()
+  }, [])
+
   return (
     <div className="flex flex-col flex-1 min-h-screen">
       <Document id={id} />
