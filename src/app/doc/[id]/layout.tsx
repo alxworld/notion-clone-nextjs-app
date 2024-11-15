70d8a151482afdadc5a24bfda0ac0f2eb6ec9ca1
alxworld <jsalex@gmail.com>
2024-11-15 22:35:32 +0530

    params fix

--- src/app/doc/[id]/layout.tsx
+++ src/app/doc/[id]/layout.tsx
@@ -4,9 +4,10 @@ import RoomProvider from '@/components/RoomProvider'
 import * as React from 'react'
 
 //function DocLayout({ children, params: { id } }: { children: React.ReactNode; params: { id: string } }) {
-function DocLayout({ children, params }: { children: React.ReactNode; params: { id: string } }) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function DocLayout({ children, params }: { children: React.ReactNode; params: any }) {
   auth.protect() // this redirects to /sign-in if not authenticated
-  const { id } = params
+  const { id } = React.use(params)
   return <RoomProvider roomId={id}>{children}</RoomProvider>
 }
 export default DocLayout
