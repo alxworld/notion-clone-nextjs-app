c5ff5caf4384c951350c506e6772cd2f6ea18e83
alxworld <jsalex@gmail.com>
2024-11-16 13:08:16 +0530

    params from client not server

--- src/app/doc/[id]/page.tsx
+++ src/app/doc/[id]/page.tsx
@@ -3,24 +3,15 @@ import * as React from 'react'
 
 import Document from '@/components/Document'
 
-type tParams = Promise<{ id: string }>
+type tParams = { id: string }
 
 function DocumentPage({ params }: { params: tParams }): JSX.Element {
   //function DocumentPage({ params: tParams }: { params: tParams }): JSX.Element {
   //const idRef = React.useRef<string | null>(null)
-  const idRef = React.useRef<string | null>(null)
-
-  React.useEffect(() => {
-    async function fetchData() {
-      const fetchedId = await params
-      idRef.current = fetchedId.id
-    }
-    fetchData()
-  }, [params])
 
   return (
     <div className="flex flex-col flex-1 min-h-screen">
-      <Document id={idRef.current || ''} />
+      <Document id={params.id || ''} />
     </div>
   )
 }
