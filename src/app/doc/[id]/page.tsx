c81530d934fb37b8cce541f7f6df2b611b7c2686
alxworld <jsalex@gmail.com>
2024-11-16 13:31:34 +0530

    params updated

--- src/app/doc/[id]/page.tsx
+++ src/app/doc/[id]/page.tsx
@@ -1,18 +1,30 @@
 'use client'
 import * as React from 'react'
-
 import Document from '@/components/Document'
+import { useEffect, useState } from 'react'
 
 type tParams = { id: string }
 
-function DocumentPage({ params }: { params: tParams }): JSX.Element {
-  //function DocumentPage({ params: tParams }: { params: tParams }): JSX.Element {
-  //const idRef = React.useRef<string | null>(null)
+function DocumentPage({ params }: { params: Promise<tParams> }): JSX.Element {
+  const [resolvedParams, setResolvedParams] = useState<tParams | null>(null)
+
+  useEffect(() => {
+    async function resolveParams() {
+      const unwrappedParams = await params // Unwrap the Promise
+      setResolvedParams(unwrappedParams)
+    }
+    resolveParams()
+  }, [params])
+
+  if (!resolvedParams) {
+    return <div>Loading...</div> // Handle loading state
+  }
 
   return (
     <div className="flex flex-col flex-1 min-h-screen">
-      <Document id={params.id || ''} />
+      <Document id={resolvedParams.id || ''} />
     </div>
   )
 }
+
 export default DocumentPage
